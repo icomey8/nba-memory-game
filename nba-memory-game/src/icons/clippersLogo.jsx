731f236665d986349c68b13ec8eafcbb2a549600
icomey8 <i.willcomey@gmail.com>
2024-07-16 16:48:03 -0700

    react-nba-logos uses defaultProps, which will be deprecated in the future.  Opting to manually make team logos as components.

---
+++ nba-memory-game/src/icons/clippersLogo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import clippersLogo from '../assets/clippers-logo.png'; // Adjust the path as necessary
+
+function ClippersLogo() {
+  return <img src={clippersLogo} alt="Clippers Logo" width={75} height={75}/>;
+}
+
+export default ClippersLogo;
